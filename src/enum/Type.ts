4110bb0b469953e8626abcfadcff946b5671e695
andressa.alves <andressa.alves@syonet.com>
2021-01-24 17:16:21 -0300

    feat(type): create router to type

---
+++ src/enum/Type.ts
@@ -0,0 +1,6 @@
+export enum Type {
+  VEGETABLES = 'Vegetais',
+  FRUITS = 'Frutas',
+  FARM_PRODUCTS = 'Naturais',
+  HANDMADE = 'Coloniais',
+}
---
+++ src/routes/Type.route.ts
@@ -0,0 +1,27 @@
+import { Router, Request, Response, NextFunction } from 'express';
+
+import { Type } from '../enum/Type';
+
+const routes = Router();
+
+function logRequest(request: Request, _response: Response, next: NextFunction) {
+  const { method, originalUrl } = request;
+  console.info(method + ': ' + originalUrl);
+  return next();
+}
+
+routes.use(logRequest);
+
+routes.get('/', async (_request, response) => {
+  try {
+    const types = Object.values(Type);
+
+    return response.json(types);
+  } catch (err) {
+    // log erro
+    console.error(err);
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export { routes as typeRoute };
